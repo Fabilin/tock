f6852a4165e2001f0c3528ac1d319abe7cebd6f2
Rodolphe Küffer <rodolphe.kuffer@partnre.com>
2026-07-29 16:48:57 +0200

    Config

--- bot/admin/web/transloco.config.ts
+++ bot/admin/web/transloco.config.ts
@@ -4,8 +4,17 @@ const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'src/assets/i18n/',
   langs: ['en', 'fr'],
   scopePathMap: {
+    analytics: './assets/i18n/analytics',
+    applications: './assets/i18n/applications',
+    bot: './assets/i18n/bot',
+    configuration: './assets/i18n/configuration',
+    faq: './assets/i18n/faq',
     'language-understanding': './assets/i18n/language-understanding',
-    analytics: './assets/i18n/analytics'
+    metrics: './assets/i18n/metrics',
+    playground: './assets/i18n/playground',
+    quality: './assets/i18n/quality',
+    rag: './assets/i18n/rag',
+    test: './assets/i18n/test'
   },
   keysManager: {
     input: 'src',
