234316f75bd0ae552faf5e0e54b6cafb6920fa73
Rodolphe Küffer <rodolphe.kuffer@partnre.com>
2026-08-12 14:47:31 +0200

    chore(studio): drop deprecated platform-browser-dynamic, remove obsolete @types/echarts, silence CommonJS bailouts

--- bot/admin/web/src/main.ts
+++ bot/admin/web/src/main.ts
@@ -15,15 +15,15 @@
  */
 
 import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { BotAdminAppModule } from './app/bot-admin-app.module';
 import { environment } from './environments/environment';
+import { platformBrowser } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
+platformBrowser()
   .bootstrapModule(BotAdminAppModule, { applicationProviders: [provideZoneChangeDetection()] })
   .catch((err) => console.log(err));
